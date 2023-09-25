9402fb1733ddc02f80d0cd0ac1bb4b84002397c7
YN1900 <100278938+YN1900@users.noreply.github.com>
2023-09-25 20:26:29 +0800

    Create quant.sh

---
+++ scripts/quant.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J salmon
+#SBATCH -p Acluster
+#SBATCH -n 40
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+
+samps=(RNA_1ug_1 RNA_1ug_2 RNA_1ug_3 RNA_1ug_4 RNA_100ng_1 RNA_100ng_2 RNA_100ng_3 RNA_100ng_4 RNA_10ng_1 RNA_10ng_2 RNA_10ng_3 RNA_10ng_4 RNA_1ng_1 RNA_1ng_2 RNA_1ng_3 RNA_1ng_4 RNA_100pg_1 RNA_100pg_2 RNA_100pg_3 RNA_100pg_4 RNA_10pg_1 RNA_10pg_2 RNA_10pg_3 RNA_10pg_4 RNA_1pg_1 RNA_1pg_2 RNA_1pg_3 RNA_1pg_4)
+
+
+file_path=/BioII/wangjb/Backup/SequencingRuns/LLP_DATA/SHERRY/20230810/20230814/trimmed
+out_path=/BioII/wangjb/Backup/SequencingRuns/LLP_DATA/SHERRY/20230810/20230814/trimmed/Outputs/Quant_results
+
+
+
+for i in ${samps[@]}
+do 
+	/Share/home/wangjb/usr/Salmon-0.8.2_linux_x86_64/bin/salmon quant -i /BioII/wangjb/Backup/SequencingRuns/LLP_DATA/SHERRY/20230810/GenomeData/Human/salmon_index/salmon_index_nodecoys -l A \
+                -1 ${file_path}/${i}/${i}_1_trimmed.fastq.gz \
+                -2 ${file_path}/${i}/${i}_2_trimmed.fastq.gz \
+                -g /BioII/wangjb/Backup/SequencingRuns/LLP_DATA/SHERRY/20230810/GenomeData/Human/salmon_index/human_trans_gene_map.txt \
+                -p 8 \
+                -o ${out_path}/${i}
+
+done
